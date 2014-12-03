7ec6ade731c2aa6bc988dfd4eb31565c42cfc5df
Salvatore <salvy.mc@gmail.com>
2014-12-03 23:39:04 +0100

    Delete Hello_World.cpp

--- Hello-World-in-C---Il-programma-stampa-sullos-chermo-la-scritta-Hello-World/include-iostream/Hello_World.cpp
+++
@@ -1,7 +0,0 @@
-/*Hello World in C++ - Il programma stampa sullos chermo la scritta Hello World! */
-#include <iostream> // per std::cout e std::endl
-using namespace std;
-int main()
-{
-cout << "Hello World!" << endl;
- }
